1608f9c254b66c29999558a338676c185e1a09f7
RyazanovAndrey <ryazanov.andrej@gmail.com>
2025-12-26 13:16:37 +0200

    13.16

--- app/page.tsx
+++ app/page.tsx
@@ -18,16 +18,10 @@ export default function Page() {
       <Services />
       <Experience />
       <Blog />
-<<<<<<< HEAD
-      <div className="size-30 bg-primary">
-        <a href="#" className="fixed right-5 bottom-5 text-primary"><ChevronUp /></a>
-      </div>
-=======
       <Testimonials />
       <Problems />
       <Footer />
       <a href="#" className="fixed p-2 right-5 bottom-5 bg-secondary rounded-full"><ChevronUp color="black" /></a>
->>>>>>> 3d3bce81840322e395a0fc1d0248654add115cd1
     </>
   )
 }
